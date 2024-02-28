252bb7d98c2615c92e085ab020dc515312699a42
BinZ <1366304589@qq.com>
2024-02-28 23:22:04 +0800

    PV 访问量+1接口

--- weblog-module-admin/src/main/java/com/binbin/weblog/admin/event/subscriber/ReadArticleSubscriber.java
+++ weblog-module-admin/src/main/java/com/binbin/weblog/admin/event/subscriber/ReadArticleSubscriber.java
@@ -2,11 +2,14 @@ package com.binbin.weblog.admin.event.subscriber;
 
 import com.binbin.weblog.admin.event.ReadArticleEvent;
 import com.binbin.weblog.common.domain.mapper.ArticleMapper;
+import com.binbin.weblog.common.domain.mapper.StatisticsArticlePVMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 @Slf4j//订阅者 ApplicationListener<自定义事件>
@@ -14,6 +17,8 @@ public class ReadArticleSubscriber implements ApplicationListener<ReadArticleEve
 
     @Autowired
     private ArticleMapper articleMapper;
+    @Autowired
+    private StatisticsArticlePVMapper articlePVMapper;
 
     @Override
     @Async("threadPoolTaskExecutor")
@@ -28,8 +33,16 @@ public class ReadArticleSubscriber implements ApplicationListener<ReadArticleEve
         log.info("==> threadName: {}", threadName);
         log.info("==> 文章阅读事件消费成功，articleId: {}", articleId);
 
+        // 执行文章阅读量 +1
         articleMapper.increaseReadNum(articleId);
+        log.info("==> 文章阅读量 +1 操作成功，articleId: {}", articleId);
+
+        // 当日文章 PV 访问量 +1
+        LocalDate currDate = LocalDate.now();
+        articlePVMapper.increasePVCount(currDate);
+        log.info("==> 当日文章 PV 访问量 +1 操作成功，date: {}", currDate);
     }
+
 
 
 }
---
+++ weblog-module-common/src/main/java/com/binbin/weblog/common/domain/mapper/StatisticsArticlePVMapper.java
@@ -0,0 +1,20 @@
+package com.binbin.weblog.common.domain.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.binbin.weblog.common.domain.dos.StatisticsArticlePVDO;
+
+import java.time.LocalDate;
+
+public interface StatisticsArticlePVMapper extends BaseMapper<StatisticsArticlePVDO> {
+
+    /**
+     * 对指定日期的文章 PV 访问量进行 +1
+     * @param date
+     */
+    default int increasePVCount(LocalDate date) {
+        return update(null, Wrappers.<StatisticsArticlePVDO>lambdaUpdate()
+                .setSql("pv_count = pv_count + 1")
+                .eq(StatisticsArticlePVDO::getPvDate, date));
+    }
+}
